a42ec79aacbd8ca304cdb183a0189db722ae0124
Tom Gontard <tomgontard@protonmail.com>
2025-04-18 14:00:03 +0200

    Mise en place client.js pour configurer le bot

---
+++ src/config/client.js
@@ -0,0 +1,13 @@
+// src/config/client.js
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+module.exports = client;
