22180dfa380811905892f30024c75ce65f63f878
Tom Gontard <tomgontard@protonmail.com>
2025-05-29 18:06:29 +0200

    Modified UserLink model

--- src/services/models/UserLink.js
+++ src/services/models/UserLink.js
@@ -2,9 +2,11 @@
 const mongoose = require('mongoose');
 
 const UserLinkSchema = new mongoose.Schema({
-  discordId: { type: String, required: true, unique: true },
-  wallet:    { type: String, required: true, unique: true },
-  registrationNumber:    { type: Number, required: true, unique: true }, // 🔢 numéro d’ordre
+  discordId:          { type: String, required: true, unique: true },
+  wallet:             { type: String, required: true, unique: true },
+  registrationNumber: { type: Number, required: true, unique: true },
+  verified:           { type: Boolean, default: false },
+  verifiedAt:         { type: Date, default: null },
 }, { timestamps: true });
 
 module.exports = mongoose.model('UserLink', UserLinkSchema);
